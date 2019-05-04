984a7a48188b00416ed597c80d71568d4cbf48ff
Oleksandr Hubanov <alexgubanow@ukr.net>
2019-05-04 22:39:27 +0300

    have got data from alphavantage for bch, eth, ltc

---
+++ t1a.py
@@ -0,0 +1,14 @@
+#imports section
+import requests
+
+#function return Ticker Data for EUR market for passed currency
+def getTickerData(self, currency, market):
+    return requests.get('https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol='+
+    currency +'&market=EUR&apikey=VEWJ1K6W3ENPI4NB')
+
+#getting BCH tickers
+BCHrq = getTickerData(0, 'BCH')
+#getting ETH tickers
+ETHrq = getTickerData(0, 'ETH')
+#getting LTC tickers
+LTCrq = getTickerData(0, 'LTC')
